aa54cc1c17a8056810151be15e4cf6cfedec6b2d
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-26 16:00:01 -0300

    fix: mongodb connection

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import "dotenv/config";
 import express from "express";
 import { MongoClient } from "mongodb";
+import participantsRouter from "./src/routes/participants.js";
 
-const port = process.env.PORT;
-const uri = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+const DB_NAME = process.env.DB_NAME;
+const URI = `${process.env.MONGO_URI}/${DB_NAME}`;
 
 const app = express();
 app.use(express.json());
 
 async function main() {
-  const client = new MongoClient(uri);
+  const client = new MongoClient(URI);
 
-  client
-    .connect()
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(console.error)
-    .finally(() => client.close());
+  client.connect((_err) => {
+    app.db = client.db(DB_NAME);
+    app.use("/participants", participantsRouter);
+  });
 }
 
 main();
 
-app.listen(port, () => console.log("Listening on port " + port));
+app.listen(PORT, () => console.log("Listening on port " + PORT));
