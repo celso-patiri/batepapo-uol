ae802f94b826b1354d389879d595b738cd0143f9
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-26 13:10:27 -0300

    add: MongoDB driver and setup

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const port = process.env.PORT;
+const uri = process.env.MONGO_URI;
 
 const app = express();
+app.use(express.json());
 
-app.listen(process.env.PORT, () =>
-  console.log("listening on port " + process.env.PORT)
-);
+async function main() {
+  const client = new MongoClient(uri);
+
+  client
+    .connect()
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(console.error)
+    .finally(() => client.close());
+}
+
+main();
+
+app.listen(port, () => console.log("Listening on port " + port));
